6051393a2fbeabc833a43447e14cf18ea6cfbabe
Jesus Morales <jesus.morales@petroprix.com>
2024-03-20 09:15:38 +0100

    arreglando bug al editar rf

--- implementacion-microservice/src/main/java/com/petroprix/implementacionmicroservice/service/ServiceImplementacion.java
+++ implementacion-microservice/src/main/java/com/petroprix/implementacionmicroservice/service/ServiceImplementacion.java
@@ -136,6 +136,8 @@ public class ServiceImplementacion {
             rf.setEstado(requisitoFuncional.estado());
             rf.setReglas(requisitoFuncional.reglas());
             rf.setTitulo(requisitoFuncional.titulo());
+
+            requisitoFuncionalRepository.save(rf);
         });
     }
 
